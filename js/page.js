7ec6044887a6b2972c017c9ca626d03da53372e2
Peter <peterhsj@gmail.com>
2025-03-17 11:11:35 +0800

    fix: 版面調整

--- js/page.js
+++ js/page.js
@@ -17,3 +17,23 @@ function clearAction() {
     this[`${menu}tab`].classList.remove('active');
   });
 };
+
+// 代辦事項 - 押匯申請待受理
+function draftHandler() {
+  const negotiation = window.parent.document.querySelector('#negotiation');
+  const acceptNegotiation = window.parent.document.querySelector('#acceptNegotiation');
+  clearAction();
+  negotiation.classList.add('show');
+  acceptNegotiation.classList.add('active');
+  window.parent.document.querySelector('#main-content').src = `./pages/acceptNegotiationList.html`;
+};
+
+// 代辦事項 - 系統待重送
+function resendHandler() {
+  const record = window.parent.document.querySelector('#record');
+  const send = window.parent.document.querySelector('#send');
+  clearAction();
+  record.classList.add('show');
+  send.classList.add('active');
+  window.parent.document.querySelector('#main-content').src = `./pages/send.html`;
+};
